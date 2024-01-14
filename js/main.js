ccc9a7b00298f59b6576394725926cb19c94809f
Saralyn <saralynl@bu.edu>
2024-01-14 15:19:36 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const nav = document.getElementById('nav-main');
+nav.innerHTML = createNavigation();
+
+const footer = document.getElementById('footer-main');
+footer.innerHTML = createFooter(); 
+
+function createNavigation() {
+    return `
+    <img id="nav-logo" src="./images/Capture.PNG" alt="M&K Global Impact globe logo">
+    <ul class="nav-list">
+    <li class="nav-item">
+        <a href="./contact.html">Contact</a>
+    </li>
+</ul>`
+}
+
+function createFooter() {
+    return `
+<p>
+   M&K Global Impact &copy; 2023
+</p>
+<p id="footer-icons">
+    <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+</p>
+<p></p>
+`
+}
