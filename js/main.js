2728931c5fb0ccc307f41516a82bdd9a4c3b48d8
Saralyn <saralynl@bu.edu>
2024-04-04 13:44:40 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -6,20 +6,30 @@ footer.innerHTML = createFooter();
 
 function createNavigation() {
     return `
-    <img id="nav-logo" src="./images/Capture.PNG" alt="M&K Global Impact globe logo">
-    <ul class="nav-list">
+    <img id="nav-logo" src="./images/logo.PNG" alt="M&K Global Impact globe logo">
+    <!--<ul class="nav-list">
     <li class="nav-item">
         <a href="#">Contact</a>
     </li>
-</ul>`
+</ul>--!>
+`
 }
 
 function createFooter() {
     return `
 <p>
-   M&K Global Impact &copy; 2023
+   M&K Global Impact &copy; 2024
 </p>
-<p id="footer-icons">
+<div id="address">
+<p>
+    136 West Dares Beach Road #207, Prince Frederick, MD 20678
+</p>
+<p>
+    <i class="fa-solid fa-phone"></i> 301-373-0790 <br>
+    <i class="fa-solid fa-envelope"></i> mkglobalimpact@gmail.com
+</p>
+</div>
+<p id="footer-links">
     <a href="#"><i class="fa-brands fa-linkedin"></i></a>
 </p>
 <p></p>
